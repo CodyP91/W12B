ca4473218b98582a75f1defd8e710467a1235aa5
Cody <cjoel91@hotmail.com>
2023-04-04 19:01:15 -0600

    Axios2 Updates

--- js/app.js
+++ js/app.js
@@ -1,35 +1,31 @@
-// Get references to the input elements and the activity element
-const typeInput = document.getElementById("type");
-const priceInput = document.getElementById("price");
-const accessibilityInput = document.getElementById("accessibility");
-const getActivityButton = document.getElementById("getActivityButton");
-const activityElement = document.getElementById("activity");
+var typeInput = document.getElementById("type");
+var priceInput = document.getElementById("price");
+var accessibilityInput = document.getElementById("accessibility");
+var getActivityButton = document.getElementById("getActivityButton");
+var activityElement = document.getElementById("activity");
 
-// Add an event listener to the button
-getActivityButton.addEventListener("click", () => {
-  // Display a loading message to the user
+getActivityButton.addEventListener("click", function () {
+
   activityElement.textContent = "Loading...";
 
-  // Build the URL for the API request based on the user's selected options
-  let apiUrl = "http://www.boredapi.com/api/activity/";
+  var apiUrl = "http://www.boredapi.com/api/activity/";
   if (typeInput.value !== "") {
-    apiUrl += `?type=${typeInput.value}`;
+    apiUrl += "?type=" + typeInput.value;
   }
   if (priceInput.value !== "") {
-    apiUrl += `&price=${priceInput.value}`;
+    apiUrl += "&price=" + priceInput.value;
   }
   if (accessibilityInput.value !== "") {
-    apiUrl += `&accessibility=${accessibilityInput.value}`;
+    apiUrl += "&accessibility=" + accessibilityInput.value;
   }
 
-  // Make an Axios call to the Bored API with the user's selected options
   axios.get(apiUrl)
-    .then(response => {
-      // Display the activity recommendation to the user
+    .then(function (response) {
+
       activityElement.textContent = response.data.activity;
     })
-    .catch(error => {
-      // Display an error message to the user
+    .catch(function (error) {
+
       activityElement.textContent = "Sorry, there was an error getting your activity recommendation.";
       console.log(error);
     });
